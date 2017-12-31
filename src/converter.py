e162589fad64bfd0120c919bab847c096e5a1733
FranzPio <franz.piontek@herpio.de>
2017-12-31 16:57:39 +0100

    v0.11.1 -> v0.11.2: tweak UI, improve postprocessing logic, prepare for video file converting, minor code improvements

--- src/converter.py
+++ src/converter.py
@@ -31,7 +31,7 @@ class FFmpeg(QtCore.QObject):
                                       ("mp3", ".mp3")])
 
     finished = QtCore.pyqtSignal()
-    success = QtCore.pyqtSignal()
+    detection_success = QtCore.pyqtSignal(str, str)
     error = QtCore.pyqtSignal(str, str, int, tuple, bool)
 
     def __init__(self, path):
@@ -47,23 +47,25 @@ class FFmpeg(QtCore.QObject):
     def convert_audio(self):
         pass
 
-    def extract_audio(self):
-        try:
-            self.get_audio_codec()
-        except FFprobeNotFoundError as error:
-            print(error, "\n", sys.exc_info())
-            self.error.emit("Error", str(error), QtWidgets.QMessageBox.Warning, sys.exc_info(), True)
-        except FFprobeError as error:
-            print(error, "\n", sys.exc_info())
-            self.error.emit("Error", str(error), QtWidgets.QMessageBox.Critical, sys.exc_info(), True)
+    def extract_audio(self, audio_codec=None):
+        if audio_codec is not None:
+            try:
+                self.audio_codec, self.file_ext = self._detect_audio_codec()
+            except (FFprobeNotFoundError, FFprobeError) as error:
+                print(error, "\n", sys.exc_info())
+                self.error.emit("Error", str(error), QtWidgets.QMessageBox.Warning, sys.exc_info(), True)
+                return
         else:
-            subprocess.run([self.ffmpeg,
-                            "-i", self.path,
-                            "-vn",
-                            "-acodec", "copy",
-                            self.path.split(".")[0] + self.file_ext])
+            self.audio_codec = audio_codec
+            self.file_ext = self.codecs.get(audio_codec)
 
-    def get_audio_codec(self):
+        subprocess.run([self.ffmpeg,
+                        "-i", self.path,
+                        "-vn",
+                        "-acodec", "copy",
+                        self.path.split(".")[0] + self.file_ext])
+
+    def _detect_audio_codec(self):
         # TODO: testing (stdout decoding seems ewwww...)
         if self.ffprobe:
             process = subprocess.Popen([self.ffprobe,
@@ -74,9 +76,20 @@ class FFmpeg(QtCore.QObject):
                                         self.path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             stdout, stderr = process.communicate()
             if stdout:
-                self.audio_codec = stdout.decode("utf-8").strip()  # does that work on Windows?! (stdout encoding different?)
-                self.file_ext = self.codecs.get(self.audio_codec)
+                audio_codec = stdout.decode("utf-8").strip()  # does that work on Windows?! (stdout encoding different?)
+                file_ext = self.codecs.get(audio_codec)
+                return audio_codec, file_ext
             else:
                 raise FFprobeError(stderr)
         else:
             raise FFprobeNotFoundError("Couldn't find ffprobe. Make sure it's installed and in your PATH.")
+
+    def get_audio_codec(self):
+        try:
+            audio_codec, file_ext = self._detect_audio_codec()
+        except (FFprobeNotFoundError, FFprobeError) as error:
+            self.error.emit("Error", str(error), QtWidgets.QMessageBox.Warning, sys.exc_info(), True)
+        else:
+            self.detection_success.emit(self.path, audio_codec)
+        finally:
+            self.finished.emit()
